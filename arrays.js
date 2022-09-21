f0b100a54efc066862872f620d61d728974a91f0
Robert Aiken <robertsiraaiken@gmail.com>
2022-09-21 09:38:40 -0500

    Add explation on Indexpositions

--- arrays.js
+++ arrays.js
@@ -7,3 +7,4 @@ strings.pop()
 numbers.shift()
 //push adds an element(s) to the end of an array, booleans would now have 6 elements
 booleans.push(true, false)
+//An index position is the number that corresponds to an element in an array. Counting elements in an array starts at 0. The second element in the "strings" array is "Agree"
