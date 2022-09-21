6401523e7dfe2729f463ff03ceeeeedc11281555
Robert Aiken <robertsiraaiken@gmail.com>
2022-09-21 09:37:23 -0500

    Add array methods again

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var strings = ["hello", "goodbye", "Agree", "Disagree"]
 var numbers = [1,2,3,4,5]
 var booleans = [true, false, false, true]
+//pop removes the last element, which would be disagree in the strings array
+strings.pop()
+//shift removes the first element, which would be 1 in the numbers array
+numbers.shift()
+//push adds an element(s) to the end of an array, booleans would now have 6 elements
+booleans.push(true, false)
